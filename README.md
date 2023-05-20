c06063165246049e294865b007281d7ec4af659b
xeno#0666 <112126426+lextra@users.noreply.github.com>
2023-05-19 19:35:37 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# cycle-stream
-a fake stream on discord with a cycling word description.
+# Creator
+coded all by loc#0001, https://discord.gg/desperate
+
+# Cycle Stream
+Allows you to keep your account status as streaming. It also allows you to set a gif link as your image in your rpc and have 2 buttons. It is fully customizable!
+
+# REPLIT SETUP
+- Open the repl
+- Goto **secrets** and type TOKEN
+- Go back to your **discord** and grab your token
+- Then **paste** your token on the secrets in the **values** one.
---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+import random
+import asyncio
+import json
+import colorama
+import discord
+from colorama import Fore
+from discord.ext import commands
+from discord.utils import get
+import requests
+
+import keep_alive
+
+with open('config.json') as f:
+    config = json.load(f)
+    token = os.getenv('TOKEN')
+
+bot = commands.Bot('.', description='discord.gg/desperate', self_bot=True)
+
+
+def clear():
+    os.system('cls')
+
+
+clear()
+
+def init():
+    token = os.getenv("TOKEN")
+    try:
+        bot.run(token, bot=False, reconnect=True)
+        os.system('title (Activity Statuses)')
+    except discord.errors.LoginFailure:
+        print(f"{Fore.RED}[ERROR] {Fore.YELLOW}Improper token has been passed" + Fore.RESET)
+        os.system('pause >NUL')
+
+
+async def ready():
+    await bot.wait_until_ready()
+
+    statuses = ["@slitmythroat", "https://fraud.gay", "/expect", "/desperate", "/stam", "⛧", "egoist", "????", "♱", "kys", "bloody", "✡"] # change these lol.
+
+    while not bot.is_closed():
+        status = random.choice(statuses)
+
+        await bot.change_presence(status=discord.Status.do_not_disturb, activity=discord.Streaming(name=status, url="https://www.twitch.tv/oversocialized")) # it can be either twitch or yt.
+
+        await asyncio.sleep(1)
+
+
+bot.loop.create_task(ready())
+
+keep_alive.keep_alive()
+
+if __name__ == '__main__':
+    init()
